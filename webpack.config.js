dab69a2edc217ad31e8ac53b4bc0e4b8d7d3ebf6
boris novikov <dev.barbaris@gmail.com>
2021-02-20 20:37:02 +0300

    proj setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ],
+  },
+};
